76384a1ed99cd88b13c48652b7f6eca9d4537515
Divyansh3021 <divyansh3021@gmail.com>
2023-04-23 01:26:54 +0530

    first commit

---
+++ chat.py
@@ -0,0 +1,10 @@
+from keras.models import load_model
+model = load_model('chat_model.h5')
+import json
+import random
+intents = json.loads(open('intents.json').read())
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.
